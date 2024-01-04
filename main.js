4dfd722f7cc8ecf4f006986b7be6196a757a2d27
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2024-01-04 11:53:26 -0500

    practince using transitons

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 let modal = document.querySelector(".modal");
 let NoModalBtn = document.querySelector(".modal__actions button");
 let plansBtns = document.querySelectorAll(".plan button");
+// let backdrop = document.querySelector(".backdrop");
 
 NoModalBtn.addEventListener("click", () => {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  //backdrop.style.display = "none";
+  // modal.style.display = "none";
+  modal.classList.remove("open");
+  backdrop.classList.remove("open");
 });
 
 plansBtns.forEach((button) => {
   button.addEventListener("click", () => {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    //backdrop.style.display = "block";
+    // modal.style.display = "block";
+
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 });
